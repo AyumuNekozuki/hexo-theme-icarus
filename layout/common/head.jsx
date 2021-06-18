abb7851b58940a0bc497a7045fd8685e3bee1e97
AyumuNekozuki <ayumu@nekozuki.me>
2021-06-18 21:06:47 +0900

    add GoogleAdSense

--- layout/common/head.jsx
+++ layout/common/head.jsx
@@ -159,6 +159,7 @@ module.exports = class extends Component {
             <script async src="https://unpkg.com/pwacompat" crossorigin="anonymous" />
 
             <script src="https://twemoji.maxcdn.com/v/latest/twemoji.min.js" crossorigin="anonymous" />
+            <script data-ad-client="ca-pub-7271400990150352" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" />
             
         </head>;
     }
